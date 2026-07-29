e8deba45e71c2f0fb885aaf42dd5b78d53b9edbc
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-07-30 00:13:45 +0600

    Service APi added.

---
+++ admin/app/api/service/route.ts
@@ -0,0 +1,26 @@
+import { connectToDB } from "@/lib/connectToDB";
+import { ServiceSection } from "@/lib/models/Service";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await connectToDB();
+  const project = await ServiceSection.find();
+  const response = NextResponse.json(project);
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  return response;
+}
+
+export async function POST(req: Request) {
+  try {
+    const data = await req.json();
+    await connectToDB();
+    await ServiceSection.create(data);
+    return NextResponse.json({ message: "Data created" }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating data:", error);
+    return NextResponse.json(
+      { message: "Failed to create data" },
+      { status: 500 },
+    );
+  }
+}
