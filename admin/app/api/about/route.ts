7253023123efb5cb9f1680084b17ebdfcd4d15bb
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-07-30 21:00:45 +0600

    About API created.

---
+++ admin/app/api/about/route.ts
@@ -0,0 +1,28 @@
+import { connectToDB } from "@/lib/connectToDB";
+import { AboutSection } from "@/lib/models/About";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await connectToDB();
+  const project = await AboutSection.find();
+  const response = NextResponse.json(project);
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  return response;
+}
+
+export async function POST(req: Request) {
+  try {
+    const data = await req.json();
+
+    // Connect to the database
+    await connectToDB();
+    await AboutSection.create(data);
+    return NextResponse.json({ message: "Data created" }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating data:", error);
+    return NextResponse.json(
+      { message: "Failed to create data" },
+      { status: 500 },
+    );
+  }
+}
