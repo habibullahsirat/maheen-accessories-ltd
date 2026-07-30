29a834be25e9a5711634727bcf10a2288a160161
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-07-30 20:49:19 +0600

    About model added.

---
+++ admin/lib/models/About.ts
@@ -0,0 +1,57 @@
+import mongoose, { Schema } from "mongoose";
+
+//Hero Section
+const ctaSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    href: {
+      type: String,
+      trim: true,
+    },
+  },
+  { _id: false },
+);
+
+const aboutSectionSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    subtitle: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    cta: {
+      type: ctaSchema,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+delete mongoose.models.AboutSection;
+
+export const AboutSection =
+  mongoose.models.AboutSection ||
+  mongoose.model("AboutSection", aboutSectionSchema);
