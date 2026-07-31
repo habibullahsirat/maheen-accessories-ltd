e4d11c9c67a0a7e2912d8565d521044fd67f105d
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-08-01 00:25:39 +0600

    Portfolio API created.

---
+++ admin/app/api/portfolio/route.ts
@@ -0,0 +1,28 @@
+import { connectToDB } from "@/lib/connectToDB";
+import { PortfolioSection } from "@/lib/models/Portfolio";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await connectToDB();
+  const photos = await PortfolioSection.find();
+  const response = NextResponse.json(photos);
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  return response;
+}
+
+export async function POST(req: Request) {
+  try {
+    const data = await req.json();
+
+    // Connect to the database
+    await connectToDB();
+    await PortfolioSection.create(data);
+    return NextResponse.json({ message: "Data created" }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating data:", error);
+    return NextResponse.json(
+      { message: "Failed to create data" },
+      { status: 500 },
+    );
+  }
+}
