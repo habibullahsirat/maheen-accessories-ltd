f6013a31ef23675a7a74f543b09ec4b6cf73cad1
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-08-02 00:25:15 +0600

    Hero route CORS updated.

--- admin/app/api/hero/route.ts
+++ admin/app/api/hero/route.ts
@@ -2,80 +2,31 @@ import { connectToDB } from "@/lib/connectToDB";
 import { HeroSection } from "@/lib/models/Hero";
 import { NextResponse } from "next/server";
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin":
-    "https://maheen-accessories-ltd-public.vercel.app",
-  "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
-
-// export async function GET() {
-//   await connectToDB();
-//   const project = await HeroSection.find();
-//   const response = NextResponse.json(project);
-//   // response.headers.set("Access-Control-Allow-Origin", "*");
-//   response.headers.set(
-//     "Access-Control-Allow-Origin",
-//     "https://maheen-accessories-ltd-public.vercel.app",
-//   );
-//   return response;
-// }
-
-// export async function POST(req: Request) {
-//   try {
-//     const data = await req.json();
-
-//     // Connect to the database
-//     await connectToDB();
-//     await HeroSection.create(data);
-//     return NextResponse.json({ message: "Data created" }, { status: 201 });
-//   } catch (error) {
-//     console.error("Error creating data:", error);
-//     return NextResponse.json(
-//       { message: "Failed to create data" },
-//       { status: 500 },
-//     );
-//   }
-// }
-
 export async function GET() {
   await connectToDB();
-
   const project = await HeroSection.find();
-
-  return NextResponse.json(project, {
-    headers: corsHeaders,
-  });
+  const response = NextResponse.json(project);
+  // response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set(
+    "Access-Control-Allow-Origin",
+    "https://maheen-accessories-ltd-public.vercel.app",
+  );
+  return response;
 }
 
 export async function POST(req: Request) {
   try {
     const data = await req.json();
 
+    // Connect to the database
     await connectToDB();
     await HeroSection.create(data);
-
-    return NextResponse.json(
-      { message: "Created" },
-      {
-        status: 201,
-        headers: corsHeaders,
-      },
-    );
+    return NextResponse.json({ message: "Data created" }, { status: 201 });
   } catch (error) {
+    console.error("Error creating data:", error);
     return NextResponse.json(
-      { message: "Failed" },
-      {
-        status: 500,
-        headers: corsHeaders,
-      },
+      { message: "Failed to create data" },
+      { status: 500 },
     );
   }
-}
-
-export async function OPTIONS() {
-  return new NextResponse(null, {
-    status: 204,
-    headers: corsHeaders,
-  });
 }
